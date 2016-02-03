a0bf1d4823f7d276e1be47ef506882d50323de7e
Kensuke Nagae <kyanny@gmail.com>
2016-02-03 12:41:13 +0900

    Backdoor

--- app.rb
+++ app.rb
@@ -4,22 +4,32 @@ require 'pp'
 require 'net/https'
 require 'uri'
 
-get '/' do
-  'ok'
-end
-
-post '/' do
-  payload = JSON.parse(request.body.read)
-  if payload['ref_type'] == 'branch' && payload['ref'] == 'master'
+helplers do
+  def post_to_slack(ref: , repo_full_name: )
     uri = URI.parse(ENV.fetch("SLACK_INCOMING_WEBHOOK_URL"))
     https = Net::HTTP.new(uri.host, uri.port)
     https.use_ssl = true
     req = Net::HTTP::Post.new(uri.request_uri)
     data = {
-        text: "#{payload['ref']} branch of <https://github.com/#{payload["repository"]["full_name"]}> is deleted. Please restore it!"
+        text: "#{ref} branch of <https://github.com/#{repo_full_name}> is deleted. Please restore it! (<https://goo.gl/3MUqah|doc>)"
     }
     req.body = data.to_json
     https.request(req)
+  end
+end
+
+get '/' do
+  'ok'
+end
+
+post '/' do
+  if params['q'] == ENV['BACKDOOR_KEY']
+    post_to_slack(ref: 'test', repo_full_name: 'kyanny/test')
+  else
+    payload = JSON.parse(request.body.read)
+    if payload['ref_type'] == 'branch' && payload['ref'] == 'master'
+      post_to_slack(ref: payload['ref'], repo_full_name: payload['repository']['full_name'])
+    end
   end
   'ok'
 end
